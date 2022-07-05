45bdead0236ac14684532884de58e90e5fd97bbb
lintricker <alina.ishem01@gmail.com>
2022-07-05 17:39:07 +0400

    task3: react-routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import MainHeader from './Components/Header/Header';
 import LeaguePage from './Components/LeaguePage/LeaguePage';
+
+import {AppBar, Container, Toolbar} from "@mui/material";
+import {Typography} from "./Components/Header/Typography";
+import {Button} from "./Components/Header/NavButton";
+import { Routes, Route, Link} from "react-router-dom";
+import { Teams } from './Components/Teams/Teams';
 
 function App() {
   return (
     <div className="App">
-      <MainHeader />
-      <LeaguePage />
+        <header>
+            <AppBar position="static">
+                <Container>
+                    <Toolbar disableGutters>
+                        <Typography variant="h6" noWrap>LOGO</Typography>
+                        <div>
+                            <Button><Link to="/">Лиги</Link></Button>
+                        </div>
+                        <div>
+                            <Button><Link to="/teams">Команды</Link></Button>
+                        </div>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </header>
+        <Routes>
+            <Route path="/" element={<LeaguePage />} />
+            <Route path="/teams" element={<Teams />} />
+        </Routes>
     </div>
   );
 }
--- src/Components/LeagueCard/LeagueCard.tsx
+++ src/Components/LeagueCard/LeagueCard.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import {CardActionArea, CardContent, Typography} from "@mui/material";
 import {Card} from './Card';
+import {Route, Routes,Link} from "react-router-dom";
+import { LeagueTimetable } from '../LeagueTimetable/LeagueTimetable';
 
 const LeagueCard= () => {
     return(
-        <div className='container'>
-        <Card>
-            <CardActionArea>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        World ChampionShip
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Russia
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-        </div>
-    )
+        <><Link to="/timetable">
+            <div className='container'>
+                <Card>
+                    <CardActionArea>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                World ChampionShip
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Russia
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </div>
+        </Link><Routes>
+            <Route path="/timetable" element={<LeagueTimetable/>}/>
+        </Routes></>
+)
 }
 export default LeagueCard;
